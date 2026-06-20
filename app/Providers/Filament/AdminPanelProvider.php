2af62c0c00cab4a556d3d5dec65af8cdb6e35929
Rizky Dhani <rizkydhni03@gmail.com>
2026-06-21 02:43:49 +0700

    Add profile() to admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
             ->maxContentWidth(Width::Full)
             ->navigationItems([
                 NavigationItem::make('Go to Site')
